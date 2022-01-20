1733f328ffca160c9656e60d29b9bfb00e8b6ca3
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-01-20 17:43:11 -0600

    REMOVED SQL logic from endpoints

--- app/main_sqla.py
+++ app/main_sqla.py
@@ -30,28 +30,13 @@ def get_posts(db: Session = Depends(get_db)):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post):
-    # Staged changes
-    cursor.execute(
-        """INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING *;""",
-        (post.title, post.content, post.published),
-    )
-    new_post = cursor.fetchone()
 
-    # Commiting change to DB
-    conn.commit()
     return {"data": new_post}
-
-
-@app.get("/posts/latest")
-def get_latest_post():
-    post = myposts[len(myposts) - 1]
-    return {"data": post}
 
 
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
-    cursor.execute("""SELECT * FROM posts WHERE id = %s;""", (str(id)))
-    post = cursor.fetchone()
+
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -62,9 +47,7 @@ def get_post(id: int, response: Response):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *;""", (str(id)))
-    deleted_post = cursor.fetchone()
-    conn.commit()
+
     if deleted_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -75,12 +58,7 @@ def delete_post(id: int):
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    cursor.execute(
-        """UPDATE posts SET title = %s, content = %s, published = %s  WHERE id = %s RETURNING *;""",
-        (post.title, post.content, post.published, str(id)),
-    )
-    updated_post = cursor.fetchone()
-    conn.commit()
+
     if updated_post == None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
