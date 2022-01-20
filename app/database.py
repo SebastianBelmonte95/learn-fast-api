709e79b5ab0c752dddc06c0cf0b3840306d0b481
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-01-20 16:36:10 -0600

    ADD SQLAlchemy engine connection

--- app/database.py
+++ app/database.py
@@ -1,3 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import json
+
+f = open("config.json")
+connection_data = json.load(f)
+f.close()
+SQLALCHEMY_DATABASE_URL = f"postgresql://{connection_data['user']}:{connection_data['password']}@{connection_data['host']}/{connection_data['database']}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
