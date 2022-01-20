a30b99bb397b81307550e103ed75cff35ba39ff6
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-01-20 17:53:01 -0600

    UPDATE create_post endpoint to use unpacked schema dict

--- app/main_sqla.py
+++ app/main_sqla.py
@@ -29,9 +29,7 @@ def get_posts(db: Session = Depends(get_db)):
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_post(post: Post, db: Session = Depends(get_db)):
-    new_post = models.Post(
-        title=post.title, content=post.content, published=post.published
-    )
+    new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
