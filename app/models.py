3caf7308c47477369ed79a73ef011b43ac1a2343
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-01-20 16:43:55 -0600

    ADD Post class

---
+++ app/models.py
@@ -0,0 +1,10 @@
+from database import Base
+from sqlalchemy import Column, Integer, Boolean, String
+
+
+class Post(Base):
+    __tablename__ = "posts"
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, primary_key=False, nullable=False)
+    content = Column(String, primary_key=False, nullable=False)
+    published = Column(Boolean, primary_key=False, nullable=False, default=True)
