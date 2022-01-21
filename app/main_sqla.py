dd06becb84150eb3edd815dbba8bfce751058bea
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-01-20 18:03:09 -0600

    UPDATE get_post by id endpoint to use SQLAlchemy

--- app/main_sqla.py
+++ app/main_sqla.py
@@ -37,8 +37,8 @@ def create_post(post: Post, db: Session = Depends(get_db)):
 
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
