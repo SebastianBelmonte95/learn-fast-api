45734ff0a29a3e29167d92917f51e2420b3e8615
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-14 00:38:42 -0600

    Renamed id to user_id

--- app/oauth2.py
+++ app/oauth2.py
@@ -22,9 +22,9 @@ def create_access_token(data: dict):
 def verify_access_token(token: str, credentials_exception):
     try:
         payload = jwt.decode(token=token, key=SECRET_KEY, algorithms=[ALGORITHM])
-        id: str = payload.get("user_id")
+        user_id: str = payload.get("user_id")
 
-        if id is None:
+        if user_id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
     except JWTError:
