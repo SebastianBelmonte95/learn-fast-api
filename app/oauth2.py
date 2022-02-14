c6175824b18a8142d9eccc8e22d0c04d5d47eff4
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-13 23:43:29 -0600

    Added create_access_token func

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,5 +1,15 @@
+from base64 import encode
 from jose import JWTError, jwt
+from datetime import datetime, timedelta
 
 SECRET_KEY = "1a9e4b835f98e902d0d8cb5006e61efed716b95451533b8b1a43140beea272e9"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(claims=to_encode, key=SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
