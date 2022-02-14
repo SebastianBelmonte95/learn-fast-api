31c40b0c152097ff22085b548139fb05c81d4dee
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-13 18:54:32 -0600

    Added verify function and docstrings

--- app/utils.py
+++ app/utils.py
@@ -4,4 +4,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash(password: str):
+    """This function hashes a string (password) and returns the encoded string"""
     return pwd_context.hash(password)
+
+
+def verify(plain_password, hashed_password):
+    """This function verifiers that the hgashed plain password matches with the hashed password in our DB"""
+    return pwd_context.verify(plain_password, hashed_password)
