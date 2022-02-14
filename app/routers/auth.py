9c32e7c73d6672a7ee0206adbdf52734511676d1
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-13 23:53:12 -0600

    Updated user_credentials w/ OAuth2PasswordRequestForm

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oauth2
 
@@ -6,10 +7,13 @@ router = APIRouter(tags=["Authentication"])
 
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
     user = (
         db.query(models.User)
-        .filter(models.User.email == user_credentials.email)
+        .filter(models.User.email == user_credentials.username)
         .first()
     )
     if not user:
