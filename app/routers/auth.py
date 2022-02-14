8c0af1f2a4b4a758d3c027811485fc7095bfcf1a
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-13 23:45:59 -0600

    Replaced example token with functional token

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
-from .. import database, schemas, models, utils
+from .. import database, schemas, models, utils, oauth2
 
 router = APIRouter(tags=["Authentication"])
 
@@ -22,5 +22,6 @@ def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.ge
         )
 
     # Create a token
+    acces_token = oauth2.create_access_token(data={"user_id": user.id})
     # Return token
-    return {"token": "example token"}
+    return {"token": acces_token, "token_type": "bearer"}
