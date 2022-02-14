a2929433834ce69b39530e799a1caaf977f07c30
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-14 00:11:48 -0600

    Fix verify_access_token not returning anything

--- app/oauth2.py
+++ app/oauth2.py
@@ -29,6 +29,7 @@ def verify_access_token(token: str, credentials_exception):
         token_data = schemas.TokenData(id=id)
     except JWTError:
         raise credentials_exception
+    return token_data
 
 
 def get_current_user(token: str = Depends(oaut2_scheme)):
