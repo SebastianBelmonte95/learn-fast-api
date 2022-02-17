22845445a8b96160f0d9a11bc9c6ec973b70baee
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-17 13:16:48 -0600

    Added owner data to Post schema

--- app/schemas.py
+++ app/schemas.py
@@ -3,6 +3,20 @@ from typing import Optional
 from pydantic import BaseModel, EmailStr
 
 
+class UserOut(BaseModel):
+    id: int
+    email: EmailStr
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
+
 class PostBase(BaseModel):
     title: str
     content: str
@@ -20,26 +34,13 @@ class Post(PostBase):
     id: int
     created_at: datetime
     user_id: int
+    owner: UserOut
 
     class Config:
         orm_mode = True
 
 
 class UserCreate(BaseModel):
-    email: EmailStr
-    password: str
-
-
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        orm_mode = True
-
-
-class UserLogin(BaseModel):
     email: EmailStr
     password: str
 
