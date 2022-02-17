2814728523451d7bccf66537ed955274be22d2a6
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-17 13:24:22 -0600

    Added limit query param to get_posts endpoint

--- app/routers/post.py
+++ app/routers/post.py
@@ -18,9 +18,11 @@ router = APIRouter(prefix="/posts", tags=["posts"])
 
 @router.get("/", response_model=List[schemas.Post])
 def get_posts(
-    db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user)
+    db: Session = Depends(get_db),
+    current_user: int = Depends(oauth2.get_current_user),
+    limit: int = 10,
 ):
-    posts = db.query(models.Post).all()
+    posts = db.query(models.Post).limit(limit).all()
     return posts
 
 
