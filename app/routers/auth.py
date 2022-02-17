79de4f30c81a7c4677c4d5f27562c47b18769b14
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-16 23:24:21 -0600

    Removed commented code

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -11,7 +11,6 @@ def login(
     user_credentials: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(database.get_db),
 ):
-    # user_credentials are not passed through a json but through form-data in Postman
     user = (
         db.query(models.User)
         .filter(models.User.email == user_credentials.username)
