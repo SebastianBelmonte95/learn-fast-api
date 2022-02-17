d338f7413e8fe1a2437eaa038d49cbb41a8a4011
Sebastian Belmonte <sebastianbelmonte95@gmail.com>
2022-02-17 16:47:04 -0600

    Added vote post endpoint

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -11,8 +11,25 @@ def vote(
     db: Session = Depends(database.get_db),
     current_user: schemas.UserOut = Depends(oauth2.get_current_user),
 ):
-
+    vote_query = db.query(models.Vote).filter(
+        models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
+    )
+    found_vote = vote_query.first()
     if vote.direction == 1:
-        db.query(models.Vote).filter(
-            models.Vote.post_id == vote.post_id, models.Vote.user_id == current_user.id
-        )
+        if found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail=f"User {current_user} has already voted on post {vote.post_id}",
+            )
+        new_vote = models.Vote(post_id=vote.post_id, user_id=current_user.id)
+        db.add(new_vote)
+        db.commit()
+        return {"message": "Succesfully added vote!"}
+    else:
+        if not found_vote:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail=f"Vote does not exist"
+            )
+        vote_query.delete(synchronize_session=False)
+        db.commit()
+        return {"message": "Succesfully deleted vote!"}
